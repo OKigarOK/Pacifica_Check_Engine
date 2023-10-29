e3c95c29d466720c36e706d5924143374413a826
OKigarOK <91433816+OKigarOK@users.noreply.github.com>
2023-10-29 13:17:47 +0300

    Add files via upload

--- accordion.js
+++ accordion.js
@@ -3,30 +3,22 @@ function ACCORD() {
     const list = document.querySelectorAll('.accordion.error_article')
     list.forEach(item => {
         item.addEventListener('click', (e) => {
+
             list.forEach(el => {
                 el.classList.remove('active');
                 const panel = el.nextElementSibling;
                 panel.style.maxHeight = null;
             });
+
             item.classList.add('active');
             const panel = item.nextElementSibling;
             panel.style.maxHeight = panel.scrollHeight + "px";
+            console.log(panel.style.maxHeight);
+
+            item.scrollIntoView({
+                behavior: "smooth",
+                block: 'nearest'
+            })
         })
     })
-}
-
-function scrollToUP() {
-    const smoothLinks = document.querySelectorAll('.error_card');
-    for (let smoothLink of smoothLinks) {
-        smoothLink.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            console.log(smoothLink);
-
-            smoothLink.scrollIntoView({
-                behavior: "smooth",
-                block: 'start'
-            })
-        });
-    }
 }
